0ae7e0ebc078d981f2833a768c51b725eca3c035
Samuel Janek <samueljanek17@gmail.com>
2023-11-27 13:30:39 +0100

    Project customer do NoSQL database on create

--- ReadService/Subscribers/CustomerSubscriber.cs
+++ ReadService/Subscribers/CustomerSubscriber.cs
@@ -1,16 +1,40 @@
 ﻿using Common.RabbitMQ;
+using Common.RabbitMQ.Messages;
+using MongoDB.Driver;
 using RabbitMQ.Client;
+using ReadService.Data.Models;
 
 namespace ReadService.Subscribers;
 
 public class CustomerSubscriber : RabbitMQReceiver<CustomerSubscriber>
 {
-    public CustomerSubscriber(IModel channel, ILogger<CustomerSubscriber> logger) : base(channel, logger)
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public CustomerSubscriber(IModel channel, ILogger<CustomerSubscriber> logger, IServiceScopeFactory serviceScopeFactory)
+        : base(channel, logger)
     {
+        _serviceScopeFactory = serviceScopeFactory;
     }
 
     protected override void HandleCreate(RabbitMQMessage message)
     {
-        Logger.LogInformation("Sevaaa");
+        using (var scope = _serviceScopeFactory.CreateScope())
+        {
+            var database = scope.ServiceProvider.GetRequiredService<IMongoDatabase>();
+
+            var data = (message.Data as CreateCustomerMessage)!;
+
+            var collection = database.GetCollection<Customer>(nameof(Customer));
+
+            var document = new Customer()
+            {
+                Email = data.Email,
+                FirstName = data.FirstName,
+                LastName = data.LastName,
+                PhoneNumber = data.PhoneNumber
+            };
+
+            collection.InsertOne(document);
+        }
     }
 }
